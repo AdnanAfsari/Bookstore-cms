c0c5cfd6fac74afefa377072699795ab6b74d14a
James Okunlade <james.okunlade@gmail.com>
2019-08-26 16:06:35 +0100

    styling added to UI

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,6 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createBook } from '../actions';
 
+const styles = {
+  input: {
+    marginRight: "10px",
+    height: "45px",
+    width: "60%",
+    fontSize: "18px"
+  },
+  select: {
+    marginRight: "10px",
+    height: "50px",
+    width: "20%",
+    fontSize: "18px"
+  },
+  button: {
+    padding: "10px",
+    width: "18%",
+    background: "#0290ff",
+    color: "#fff",
+    padding: "16px",
+    fontSize: "18px"
+  }
+}
 
 
 const bookCategories = [
@@ -63,11 +85,11 @@ class BooksForm extends React.Component {
 
     return (
       <div>
-        <h3>Books form:</h3>
+        <h3>ADD NEW BOOK</h3>
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="text" name="title" />
-          <select onChange={this.handleChange} name="category">{bookOptions}</select>
-          <input type="submit" />
+          <input onChange={this.handleChange} style={styles.input} type="text" name="title" />
+          <select onChange={this.handleChange} style={styles.select} name="category">{bookOptions}</select>
+          <input style={styles.button} type="submit" value="ADD BOOK"/>
         </form>
       </div>
     )
