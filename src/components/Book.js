f379e9d8d931ee63bfb7099fa3ae33e49def1d19
Adnan Afsari <aarahul1991@gmail.com>
2019-08-26 19:24:36 +0600

    remove book actione connected to components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,24 +1,24 @@
 import React from 'react';
-
+​
 class Book extends React.Component {
   render() {
     const book = this.props.book;
-
+​
     return(
       <tr>
         <td>{book.id}</td>
         <td>{book.title}</td>
         <td>{book.category}</td>
         <td>
-        remove
+          <button onClick={() => this.props.handleBookRemove(book)}>Remove Book</button>
         </td>
       </tr>
     )
   }
 }
-
-
-
-
-
+​
+​
+​
+​
+​
 export default Book;
