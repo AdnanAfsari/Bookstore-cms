a4a5a5c498cfbf49e6f510c2dc592bb12d384d3c
Adnan Afsari <aarahul1991@gmail.com>
2019-08-26 21:17:17 +0600

    styling completed

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import classes from './Book.css';
 class Book extends React.Component {
   render() {
     const book = this.props.book;
-
     return(
-      <tr>
-        <td>{book.id}</td>
-        <td>{book.title}</td>
-        <td>{book.category}</td>
-        <td>
-          <button onClick={() => this.props.handleBookRemove(book)}>Remove Book</button>
-        </td>
-      </tr>
+      <div className="panel">
+        <div className="first">
+          <h5>{book.category}</h5>
+          <h2>{book.title}</h2>
+          <p>Suzanne Collins</p>
+          <div className="meta-panel">
+            <div className="meta">Comments</div>
+            <div className="meta"><button onClick={() => this.props.handleBookRemove(book)}>Remove Book</button></div>
+            <div className="meta">Edit</div>
+          </div>
+        </div>
+        <div className="second"></div>
+        <div className="third">
+          <h5>CURRENT CHAPTER</h5>
+          <h5> CHAPTER 17</h5>
+          <button>UPDATE PROGRESS</button>
+        </div>
+      </div>
     )
   }
 }
-
-
 export default Book;
