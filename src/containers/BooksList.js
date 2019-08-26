d431d7a1a7004ab1647c767dc0608b7915d22665
Adnan Afsari <aarahul1991@gmail.com>
2019-08-26 20:45:33 +0600

    Keep the filter value in the Redux store

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Book from '../components/Book'
-import { removeBook } from '../actions'
-
-
-
+import { changeFilter, removeBook } from '../actions'
+import CategoryFilter from '../components/CategoryFilter';
+​
+​
+​
+const filteredBooks = (books, filter) => {
+  if(filter === 'All') {
+    return books;
+  } else {
+    return books.filter((book) => {
+      return book.category === filter;
+    });
+  }
+}
+​
+​
 class BooksList extends React.Component {
   constructor(props) {
     super(props);
-
+​
+    this.handleFilterChange = this.handleFilterChange.bind(this);
     this.handleBookRemove = this.handleBookRemove.bind(this);
   }
-
+​
+  handleFilterChange(event) {
+    this.props.changeFilter(event.target.value);
+  }
+​
   handleBookRemove(book) {
     this.props.removeBook(book)
   }
-
+​
   render() {
-    const books = this.props.books.map((book) => {
+    const books = filteredBooks(this.props.books, this.props.filter).map((book) => {
       return <Book key={book.id} book={book} handleBookRemove={this.handleBookRemove} />;
     });
-
+​
     return(
       <div>
+      <CategoryFilter handleFilterChange={this.handleFilterChange}/>
         <table>
           <thead>
             <tr>
@@ -39,18 +57,20 @@ class BooksList extends React.Component {
     )
   }
 }
-
+​
 const mapStateToProps = (state, ownProps) => {
   return {
-    books: state.books
+    books: state.books,
+    filter: state.filter
   }
 };
-
+​
 const mapDispatchToProps = (dispatch) => {
   return {
+    changeFilter: filter => dispatch(changeFilter(filter)),
     removeBook: book => dispatch(removeBook(book))
   }
 };
-
-
+​
+​
 export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
